a687cb391d59cc8e6ce51f5b06ee3eaf443d4fd5
shobhit singhal <singhalshobhit12@gmail.com>
2024-12-30 16:37:46 +0530

    fixed width

--- employeeManagement/src/components/TaaskStats.jsx
+++ employeeManagement/src/components/TaaskStats.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TaaskStats = (props) => {
   return (
-    <div className='border-2 py-3 w-[49%]  rounded-md flex flex-col items-start px-3 gap-3 bg-transparent '
+    <div className='border-2 py-3 w-[48%]  rounded-md flex flex-col items-start px-3 gap-3 bg-transparent '
     style={props.styles}
     >
       <h1 className='font-bold text-2xl text-gray-600'>{props.val}</h1>
